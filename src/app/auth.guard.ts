27dd5041860a40359c150f3c502a392c39235d22
czaro <cezarykalinowski05@gmail.com>
2025-05-19 17:39:52 +0200

    add register, login screens and authentication logic

---
+++ src/app/auth.guard.ts
@@ -0,0 +1,14 @@
+import {CanActivateFn, Router} from '@angular/router';
+import {AuthService} from './auth.service';
+import {inject} from '@angular/core';
+import {map, take} from 'rxjs';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService)
+  const router = inject(Router)
+
+  return authService.isAuthenticated().pipe(
+    take(1),
+    map(isAuth => isAuth ? true : router.createUrlTree(['/login']))
+  );
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {RouterModule} from '@angular/router';
+import {routes} from './app/app.routes';
+import {importProvidersFrom} from '@angular/core';
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
+import {authInterceptor} from './app/authInterceptor';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(RouterModule.forRoot(routes)),
+    provideHttpClient(withInterceptors([authInterceptor])),
+  ]
+})
   .catch((err) => console.error(err));
